1c04794eefe0c58f77659d3e3a6cb0c3b5b60b5f
rory cornell <rory@rorycornell.com>
2017-03-11 12:44:43 -0500

    added bootstrap theme

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import urllib2
 from bs4 import BeautifulSoup
 app = Flask(__name__)
 
+
+
+
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
                 'cnn': 'http://rss.cnn.com/rss/edition.rss',
                 'fox': 'http://feeds.foxnews.com/foxnews/latest',
